33fd83fb04438f7dbb390ec338967a541f7282a3
Yury Layeuski <ylayeuski@ylayeuski-mbp.local>
2016-07-11 20:46:06 -0700

    Implement articles#index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,18 @@
 class ArticlesController < ApplicationController
 
+def index
+  @articles = Article.all
+end
+
+
 def show
   @article = Article.find(params[:id])
 end
 
 
-
 def new
 end
+
 
 def create
       @article = Article.new(article_params)
