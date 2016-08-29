863074a05d9533ccf2da8bba3f39c92df81861f7
Yurkapur <yurkapur83@gmail.com>
2016-08-28 21:51:31 -0700

    Update article factory

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,14 @@ FactoryGirl.define do
   factory :article do 
     title "Article title"
     text "Article text"
+      #create factory with name 'article_with_comments'
+      #for creating article with couple comments
+    factory :article_with_comments do
+        #after creating article 
+      after :create do |article, evaluator|
+          #create list with 3 comments
+        create_list :comment, 3, article: article
+      end
+    end
   end
 end
