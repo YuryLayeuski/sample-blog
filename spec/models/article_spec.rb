bcf396554ac31ce833a6b53bb8527c8f184e4a01
Yurkapur <yurkapur83@gmail.com>
2016-08-28 21:28:09 -0700

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,5 +2,23 @@ require "rails_helper"
 
 
 RSpec.describe Article, type: :model do
-  it { should have_many(:comments) }
- end
+  describe "validations" do
+    it { should validate_presence_of :title}
+    it { should validate_presence_of :text}
+  end
+
+  describe "associations" do
+    it { should have_many(:comments) }
+  end
+
+  describe "#subject" do 
+    it "returns the article title" do
+      #create object article tricky way
+
+      article = create(:article, title: 'Lorem Ipsum')
+
+      #assert
+      expect(article.subject).to eq 'Lorem Ipsum'
+    end
+  end
+end
